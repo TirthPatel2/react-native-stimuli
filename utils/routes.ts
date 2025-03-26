34d6649cea4d4d11ebc05865bb2b307c7743c7c3
Tirth <tirthpatel7563@gmail.com>
2025-03-26 20:24:57 +0530

    stopwatch with animation

--- utils/routes.ts
+++ utils/routes.ts
@@ -1,7 +1,9 @@
 import HomeScreen from "@/app/index";
 import ResultScreen from "@/app/Result";
+import TimerAnimation from "@/app/TimerAnimation";
 
 export const routes = [
     { name: "Home", label: "Home", component: HomeScreen, icon: "home" },
     { name: "Result", label: "Saved", component: ResultScreen, icon: "content-save-all" },
+    { name: "Timer Animation", label: "Animation", component: TimerAnimation, icon: "timelapse" },
 ];
